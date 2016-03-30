2e6322f8d7f1e79dbcabd0d0a4841dc4857e246a
Duncan McGreggor <oubiwann@gmail.com>
2016-03-30 18:33:11 -0500

    Added unit test for xml-request?.
    Also cleaned up tests a bit.

--- test/clojusc/ring/test/xml.clj
+++ test/clojusc/ring/test/xml.clj
@@ -3,12 +3,27 @@
         clojure.test
         ring.util.io))
 
-(deftest test-xml-request
+(def request-1 {:content-type "application/xml"
+                :body (string-input-stream "<xml></xml>")})
+
+(def request-2 {:content-type "text/xml"
+                :body (string-input-stream "<xml></xml>")})
+
+(def request-3 {:content-type "application/xml"
+                :body (string-input-stream "<xml></XML>")})
+
+(def request-4 {:content-type "text/plain"
+                :body (string-input-stream "stuff")})
+
+(deftest test-xml-request?
+  (is (xml-request? request-1))
+  (is (xml-request? request-2))
+  (is (not (xml-request? request-4))))
+
+(deftest test-xml-request-body
   (let [handler (wrap-xml-request identity)]
     (testing "xml body"
-      (let [request   {:content-type "application/xml"
-                       :body (string-input-stream "<xml></xml>")}
-            response  (handler request)]
+      (let [response  (handler request-1)]
         (is (= [{:tag :xml :attrs nil :content nil} nil] (:body response)))))))
 
 (deftest test-xml-response
@@ -20,10 +35,8 @@
       (is (= (:body response) "<xml/>")))))
 
 (deftest test-invalid-xml-returns-400
-  (let [handler (wrap-xml-request identity)]
-      (let [request   {:content-type "application/xml"
-                       :body (string-input-stream "<xml></XML>")}
-            response  (handler request)]
-      (is (= (:status response) 400))
-      (is (= (get-in response [:headers "Content-Type"]) "text/plain"))
-      (is (= (:body response) "The element type \"xml\" must be terminated by the matching end-tag \"</xml>\".")))))
+  (let [handler (wrap-xml-request identity)
+        response  (handler request-3)]
+    (is (= (:status response) 400))
+    (is (= (get-in response [:headers "Content-Type"]) "text/plain"))
+    (is (= (:body response) "The element type \"xml\" must be terminated by the matching end-tag \"</xml>\"."))))
