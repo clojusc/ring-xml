76a8945011cb2bbdf4da775b759b0b1b0bb799fc
Duncan McGreggor <oubiwann@gmail.com>
2016-03-30 18:02:52 -0500

    Updated project metadata.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject imintel/ring-xml "0.0.3" 
-  :description "Ring middleware for handling XML."
-  :url "https://github.com/imintel/ring-xml"
+(defproject clojusc/ring-xml "0.0.3"
+  :description "Ring middleware for XML requests and responses."
+  :url "https://github.com/clojusc/ring-xml"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
