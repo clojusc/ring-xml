3d3798b7126f98efc765cefc89618deb59132534
Duncan McGreggor <oubiwann@gmail.com>
2016-09-23 10:42:26 -0500

    Bumped version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojusc/ring-xml "0.0.6"
+(defproject clojusc/ring-xml "0.1.0"
   :description "Ring middleware for XML requests and responses."
   :url "https://github.com/clojusc/ring-xml"
   :license {:name "The MIT License"
